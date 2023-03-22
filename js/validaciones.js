dd33177f41aaf641ec797c07d4d440355a3a7b61
Natalia <nati.ch@live.com.ar>
2023-03-22 20:37:59 -0300

    Guardando validacion de formularios

---
+++ js/validaciones.js
@@ -0,0 +1,96 @@
+export function valida(input) {
+    const tipoDeInput = input.dataset.tipo;
+    if (validadores[tipoDeInput]) {
+        validadores[tipoDeInput](input);
+    }
+
+if(input.validity.valid) {
+    input.parentElement.classList.remove("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = "";
+}else {
+    input.parentElement.classList.add("input-container--invalid");
+    input.parentElement.querySelector(".input-message-error").innerHTML = mostrarMensajeDeError(tipoDeInput, input);
+}
+}
+
+
+const tipoDeErrores = [
+    "valueMissing",
+    "typeMismatch",
+    "patternMismatch",
+    "customError",
+];
+
+
+const mensajesDeError = {
+    nombre : {
+        valueMissing: "El campo nombre no puede estar vacio",
+    },
+    email : {
+        valueMissing: "El campo correo no puede estar vacio",
+        typeMismatch: "El correo no es valido" ,
+    },
+    password : {
+        valueMissing: "El campo contraseña no puede estar vacio",
+        patternMismatch: "Debe contener como minimo 8 caracteres, al menos una letra y un numero.",
+    },
+    nacimiento : {
+        valueMissing: "El campo fecha de nacimiento no puede estar vacio",
+        customError: "'Debes tener al menos 18 años de edad.",
+    },
+    numero : {
+        valueMissing: "Este campo no puede estar vacio",
+        patternMismatch: "El formato requerido es XXXXXXXXXX 10 numeros."
+    },
+    direccion : {
+        valueMissing: "Este campo no puede estar vacio",
+        patternMismatch: "La direccion debe contener entre 10 y 40 caracteres."
+    },
+    ciudad : {
+        valueMissing: "Este campo no puede estar vacio",
+        patternMismatch: "La ciudad debe contener entre 4 y 40 caracteres."
+    },
+    estado : {
+        valueMissing: "Este campo no puede estar vacio",
+        patternMismatch: "El estado debe contener entre 4 y 40 caracteres."
+    },
+}
+
+const validadores = {
+    nacimiento : (input) => validarNacimiento(input),
+}
+
+function mostrarMensajeDeError(tipoDeInput, input) {
+    let mensaje = "";
+    tipoDeErrores.forEach((error) => {
+        if(input.validity[error]) {
+            console.log(tipoDeInput, error);
+            console.log(input.validity[error]);
+            console.log(mensajesDeError[tipoDeInput][error]);
+            mensaje = mensajesDeError[tipoDeInput][error];
+        }
+        
+    });
+    return mensaje;
+}
+
+function validarNacimiento(input) {
+    const fechaCliente = new Date(input.value);
+    let mensaje = '';
+    if(!mayorDeEdad(fechaCliente)) {
+        mensaje = ('Debes tener al menos 18 años de edad.')
+    }
+
+    input.setCustomValidity(mensaje);
+}
+
+function mayorDeEdad(fecha) {
+    const fechaActual = new Date();
+    const diferenciaFechas = new Date(
+        fecha.getUTCFullYear() + 18,
+        fecha.getUTCMonth(),
+        fecha.getUTCDate(),
+    );
+    return fechaActual >= diferenciaFechas;
+
+}
